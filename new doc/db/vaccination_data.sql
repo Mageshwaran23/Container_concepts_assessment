b0d2805653e164a0e9ee45e247ee34ea0ccd381b
Mageshwaran N <84568658+Mageshwaran23@users.noreply.github.com>
2023-05-03 22:26:47 +0530

    Delete new doc directory

--- new doc/db/vaccination_data.sql
+++
@@ -1,8 +0,0 @@
-USE vaccination;
-CREATE TABLE status (
-  name CHAR(15) PRIMARY KEY,
-  regno INT,
-  status CHAR(3)
-);
-INSERT INTO student (name, regno, status) VALUES ('Magesh', 12345, 'Yes');
-INSERT INTO student (name, regno, status) VALUES ('Mathesh', 123546, 'No');
